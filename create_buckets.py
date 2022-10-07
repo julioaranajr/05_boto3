6f3c9e22cab429d3134ff61037e94a0b752f7fc1
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-07 12:36:35 +0200

    Creating a Bucket in aws s3 and Storing Data

--- create_buckets.py
+++ create_buckets.py
@@ -4,10 +4,12 @@ import boto3
 s3 = boto3.resource('s3')
 
 #Creating a Bucket
-s3.create_bucket(Bucket='ta-boto3-python-labs')
 
-s3.create_bucket(Bucket='ta-python-labs', CreateBucketConfiguration={
+s3.create_bucket(Bucket='ta-test-boto3-python-labs')
+
+s3.create_bucket(Bucket='ta-test-boto3-python-labs', CreateBucketConfiguration={
     'LocationConstraint': 'eu-central-1'})
 
 #Storing Data
-s3.Object('ta-pythonlabs', 'hello.txt').put(Body=open('/tmp/hello.txt', 'rb'))
+
+s3.Object('ta-test-boto3-python-labs', 'hello.txt').put(Body=open('/tmp/hello.txt', 'rb'))

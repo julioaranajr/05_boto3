75001acfd1f7852445f25fe9e47e893f58626094
Julio Arana Jr <julio.arana@cloudreach.com>
2022-10-11 00:16:30 +0200

    Current time of a timezone

---
+++ current_time.py
@@ -0,0 +1,19 @@
+from datetime import datetime
+import pytz
+from pytz import all_timezones
+len(all_timezones) >= 500
+
+print(pytz.country_names['US'])
+tz_NY = pytz.timezone('America/New_York') 
+datetime_NY = datetime.now(tz_NY)
+print("New York time:", datetime_NY.strftime("%H:%M:%S"))
+
+print(pytz.country_names['GB'])
+tz_London = pytz.timezone('Europe/London')
+datetime_London = datetime.now(tz_London)
+print("London time:", datetime_London.strftime("%H:%M:%S"))
+
+print(pytz.country_names['DE'])
+tz_Berlin = pytz.timezone('Europe/Berlin')
+datetime_Berlin = datetime.now(tz_Berlin)
+print("Berlin time:", datetime_Berlin.strftime("%H:%M:%S"))
